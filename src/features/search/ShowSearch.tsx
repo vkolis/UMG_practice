67c2de65643334326a6b537c25b210a89ed1a729
Volodymyr Kolisnichenko <volodymyr.kolisnichenko@dataart.com>
2026-03-09 17:16:07 +0100

    feat/connect tanstack to app

--- src/features/search/ShowSearch.tsx
+++ src/features/search/ShowSearch.tsx
@@ -1,7 +1,19 @@
+import { useQuery } from "@tanstack/react-query"
 import { SearchInput } from "./ui/SearchInput"
+import { Container } from "@mui/material"
 
 export const ShowSearch = () => {
+  const { data } = useQuery({
+    queryKey: ['films'],
+    queryFn: () => fetch('https://swapi.dev/api/films/').then(res => res.json())
+  })
+  console.log(data.results)
   return (
-    <SearchInput inputValue={""} setInputValue={() => {}} />
+    <Container>
+      <SearchInput inputValue={""} setInputValue={() => {}} />
+      {data?.results.map((film: { title: string }) => (
+        <div key={film.title}>{film.title}</div>
+      ))}
+    </Container>
   )
 }
