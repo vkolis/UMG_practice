c473134370fd3dda5785e7bd6bd054df795c75bb
Volodymyr Kolisnichenko <volodymyr.kolisnichenko@dataart.com>
2026-03-10 10:43:00 +0100

    add returned data handling

--- src/features/search/ShowSearch.tsx
+++ src/features/search/ShowSearch.tsx
@@ -1,18 +1,53 @@
 import { useQuery } from "@tanstack/react-query"
 import { Container } from "@mui/material"
 import { filmService } from "@/features/search/api"
+import type { Film } from "@/features/search/api"
 import { SearchInput } from "@/features/search/ui"
+import { useState } from "react"
 
 export const ShowSearch = () => {
-  const { data } = useQuery({
+  const [inputValue, setInputValue] = useState('')
+
+  const { data, isLoading, error } = useQuery({
     queryKey: ['films'],
-    queryFn: () => filmService.getFilms()
+    queryFn: () => filmService.getFilms(),
+    staleTime: 1000 * 60 * 10
   })
-  
+
+  const films = data?.results
+  const normalizedInput = inputValue.trim().toLowerCase()
+
+  let filteredFilms: Film[] = []
+
+  if (normalizedInput && films) {
+    filteredFilms = films
+      .filter((film) =>
+          film.title.toLowerCase().includes(normalizedInput) ||
+          film.opening_crawl.toLowerCase().includes(normalizedInput),
+      )
+      .slice(0, 6)
+  }
+
+  if (isLoading) {
+    return (
+      <Container>
+        <div>Loading...</div>
+      </Container>
+    )
+  }
+
+  if (error) {
+    return (
+      <Container>
+        <div>Error occurred while fetching films</div>
+      </Container>
+    )
+  }
+
   return (
     <Container>
-      <SearchInput inputValue={""} setInputValue={() => {}} />
-      {data?.results.map((film: { title: string }) => (
+      <SearchInput inputValue={inputValue} setInputValue={setInputValue} />
+      {filteredFilms.map((film) => (
         <div key={film.title}>{film.title}</div>
       ))}
     </Container>
