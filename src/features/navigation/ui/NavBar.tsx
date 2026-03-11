29b055699d7a7617e3390d4b8eeaa3aff4c747da
Volodymyr Kolisnichenko <volodymyr.kolisnichenko@dataart.com>
2026-03-11 15:24:40 +0100

    add icon

--- src/features/navigation/ui/NavBar.tsx
+++ src/features/navigation/ui/NavBar.tsx
@@ -1,17 +1,24 @@
+import { IconButton } from '@mui/material';
+import { Dashboard } from '@mui/icons-material';
 import Box from '@mui/material/Box';
 
 export const NavBar = () => {
   return (
-       <Box
-          sx={{
-            position: "fixed",
-            top: 0,
-            left: 0,
-            width: 50,
-            height: "100vh",
-            bgcolor: "#fff",
-            borderRight: "1px solid",
-          }}
-      />
+      <Box
+        sx={{
+          position: "fixed",
+          top: 0,
+          left: 0,
+          width: 50,
+          height: "100vh",
+          bgcolor: "#fff",
+          borderRight: "1px solid #e0e0e0",
+          boxShadow: "4px 0 16px rgba(0, 0, 0, 0.08)",
+        }}
+      >
+      <IconButton>
+        <Dashboard />
+      </IconButton>
+    </Box>
   );
 }
