737cecc61cd6bb21903311a0a1142d3810777065
Volodymyr Kolisnichenko <volodymyr.kolisnichenko@dataart.com>
2026-03-11 12:41:01 +0100

    use functions instead of class in films.service

--- src/features/search/api/films.service.ts
+++ src/features/search/api/films.service.ts
@@ -1,18 +1,16 @@
 import axios from "axios";
-import type { Film, FilmsResponse } from "./films.types";
+import type { Film, FilmsResponse } from "@/features/search/api";
 
-class FilmService {
-  private baseUrl = 'https://swapi.dev/api/films/';
+  const FILMS_URL = 'https://swapi.dev/api/films/';
 
-  async getFilms(): Promise<FilmsResponse> {
-    const res = await axios.get<FilmsResponse>(this.baseUrl)
+  const getFilms = async (): Promise<FilmsResponse> => {
+    const res = await axios.get<FilmsResponse>(FILMS_URL)
     return res.data;
   }
 
-  async getFilmByUrl(url: string): Promise<Film> {
+  const getFilmByUrl = async (url: string): Promise<Film> => {
     const res = await axios.get<Film>(url)
     return res.data
   }
-}
 
-export const filmService = new FilmService();
+export const filmService = { getFilms, getFilmByUrl };
