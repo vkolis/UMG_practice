e43d3a6d1ad28654c2dd0775430419b595953749
Volodymyr Kolisnichenko <volodymyr.kolisnichenko@dataart.com>
2026-03-11 12:17:42 +0100

    fix search logic

--- src/shared/hooks/useShowSearchState.ts
+++ src/shared/hooks/useShowSearchState.ts
@@ -31,9 +31,7 @@ export const useShowSearchState = ({ films }: UseShowSearchStateArgs) => {
 
     return films
       .filter(
-        (film) =>
-          film.title.toLowerCase().includes(normalizedInput) ||
-          film.opening_crawl.toLowerCase().includes(normalizedInput),
+        (film) => film.title.toLowerCase().includes(normalizedInput)
       )
       .slice(0, MAX_SEARCH_RESULTS)
   }, [films, normalizedInput])
