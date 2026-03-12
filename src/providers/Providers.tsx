05f3bfcf5c523c4024474041463f3b6868107c7f
Volodymyr Kolisnichenko <volodymyr.kolisnichenko@dataart.com>
2026-03-12 09:41:34 +0100

    connect default ThemeProvider

--- src/providers/Providers.tsx
+++ src/providers/Providers.tsx
@@ -1,18 +1,23 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import type { ReactNode } from "react"
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
-  const queryClient = new QueryClient({
-    defaultOptions: {
-      queries: {
-        refetchOnWindowFocus: false,
-      },
-    }
-  })
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  }
+})
+
+const theme = createTheme({});
 
 export const Providers = ({ children }: { children: ReactNode }) => {
   return (
     <QueryClientProvider client={queryClient}>
-      {children}
+      <ThemeProvider theme={theme}>
+            {children}
+      </ThemeProvider>
     </QueryClientProvider>
   )
 }
