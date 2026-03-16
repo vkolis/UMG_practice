d900549803c7c6d87f206e2fb49a76a697b305ea
Volodymyr Kolisnichenko <volodymyr.kolisnichenko@dataart.com>
2026-03-16 16:06:10 +0100

    extend form

--- src/features/aside-form/ui/AsideForm.tsx
+++ src/features/aside-form/ui/AsideForm.tsx
@@ -1,13 +1,15 @@
-import { TextField } from "@mui/material"
+import { Button, Stack, TextField } from "@mui/material"
 import { useForm, type SubmitHandler } from "react-hook-form"
 
 type AsideFormValues = {
   email: string
+  name: string
 }
 
 export const AsideForm = () => {
   const { register, handleSubmit } = useForm<AsideFormValues>({
-    mode: "onChange"
+    mode: "onChange",
+    // defaultValues: {email: "test"}
   })
 
   const onSubmit: SubmitHandler<AsideFormValues> = (data) => {
@@ -16,15 +18,29 @@ export const AsideForm = () => {
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}> 
+    <Stack spacing={2} sx={{ margin: "60px 8px 0"}}>
       <TextField 
         id="outlined-basic" 
         label="email" 
         variant="outlined"
-        sx={{ margin: "60px 8px"}}
+        type="email"
         {...register('email', {
           required: 'This field is required'
         })}
       />
+      <TextField 
+        id="outlined-basic" 
+        label="name" 
+        variant="outlined"
+        type="name"
+        {...register('name', {
+          required: 'This field is required'
+        })}
+      />
+      <Button type="submit" variant="contained">
+        Submit
+      </Button>
+    </Stack>
     </form>
   )
 }
