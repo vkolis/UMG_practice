0a5e178dc71442bfe9fae5be74acbb03bba9af4b
Volodymyr Kolisnichenko <volodymyr.kolisnichenko@dataart.com>
2026-03-16 14:18:29 +0100

    add email input

--- src/features/aside-form/ui/AsideForm.tsx
+++ src/features/aside-form/ui/AsideForm.tsx
@@ -1,7 +1,30 @@
+import { TextField } from "@mui/material"
+import { useForm, type SubmitHandler } from "react-hook-form"
+
+type AsideFormValues = {
+  email: string
+}
+
 export const AsideForm = () => {
+  const { register, handleSubmit } = useForm<AsideFormValues>({
+    mode: "onChange"
+  })
+
+  const onSubmit: SubmitHandler<AsideFormValues> = (data) => {
+    console.log(data)
+  }
+
   return (
-    <div>
-      Form
-    </div>
+    <form onSubmit={handleSubmit(onSubmit)}> 
+      <TextField 
+        id="outlined-basic" 
+        label="email" 
+        variant="outlined"
+        sx={{ margin: "60px 8px"}}
+        {...register('email', {
+          required: 'This field is required'
+        })}
+      />
+    </form>
   )
 }
